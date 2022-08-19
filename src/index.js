7c76e76ed4fe32341e57d95364222fb6859787fd
Mohd Amir <Amirkhan98375@gmail.com>
2022-08-19 22:23:03 +0530

    backend

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+//const mongoose = require('mongoose')
+const connect = require("./configs/db")
+const userController= require("./controller/user.controller")
+const User = require("./models/user.model")
+const {register,login,loggedUser,finduser} = require("./controller/auth.controller")
+const authenticate=require("./middleware/authenticate.js")
+
+
+const app = express();
+
+app.use(express.json())
+app.use("/users",userController)
+app.post("/register", register)
+app.post("/login",login)
+app.get("/allUsers",finduser)
+app.get("/loggedUser",authenticate,loggedUser)
+// app.use("/products",productController)
+
+app.listen(5000,async()=>{
+   try {
+    await connect()
+    console.log("listening on port 5000")
+   } catch (error) {
+       console.log(error.message)
+   }
+})
